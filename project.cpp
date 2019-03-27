a6e59756a052c7d5d81e61b37b890c98d7788285
Ankit123Mishra <40914510+Ankit123Mishra@users.noreply.github.com>
2019-03-28 00:30:53 +0530

    Add files via upload

--- project.cpp
+++ project.cpp
@@ -1,4 +1,3 @@
-//3324325
 #include<iostream>
 using namespace std;
 #include<queue>
@@ -9,8 +8,8 @@ int a[100][7];
 int tq=4;
 int timeRoundRobin;
 int timePriority;
+int timeFcfs;
 int time=0;
-int start=0;
 
 //queues
 queue<int*> q1;
@@ -21,17 +20,15 @@ queue<int*> qn;
 //functions
 int roundrobin();
 int priority();
+int fcfs();
 void assignQueues(int end);
 void display(queue<int*> q);
-void assignQueuesInitial();
-//void callingRR();
-//void callingPriority();
 void getdata();
 void ordering(queue<int*> q);
+void orderingfcfs(queue<int*> q);
 
 main()
 {
-	int w;
 	getdata();
     roundrobin();
 }
@@ -40,6 +37,11 @@ void display(queue<int*> q)
 {
 	queue<int*> g=q;
 	string dem="";
+	if(q.empty())
+	{
+		cout<<"\nqn-->Empty queue\n";
+	}
+	else{
 	while(!g.empty())
 	{
 		cout<<dem<<"P"<<g.front()[0];
@@ -47,6 +49,7 @@ void display(queue<int*> q)
 		g.pop();
 	}
 	cout<<"\n";
+    }
 }
 
 void getdata()
@@ -74,6 +77,15 @@ void getdata()
 		a[i][7]=0;
 		qn.push(a[i]);
 	}
+	int min=a[0][2];
+	for(int i=0;i<n;i++)
+	{
+		if(a[i][2]<min)
+		{
+			min=a[i][2];
+		}
+	}
+	time=time+min;	
 }
 
 int checkduplicates(queue<int*> q, int p)
@@ -92,6 +104,8 @@ int checkduplicates(queue<int*> q, int p)
 }
 void assignQueues(int end)
 {
+	if(!qn.empty())
+	{
 	queue<int*> q=qn;
 	queue<int*> g;
 	while(!q.empty())
@@ -115,6 +129,14 @@ void assignQueues(int end)
 					q2.push(q.front());
 				}
 			}
+			else
+			{
+				int c=checkduplicates(q3,q.front()[0]);
+				if(c==0)
+				{
+					q3.push(q.front());
+				}
+			}
 		   	f=1;
 		   	qn.pop();
 		}
@@ -126,6 +148,7 @@ void assignQueues(int end)
 		q.pop();
 	}
 	qn=g;
+    }
 }
 
 void displayProcess(int* a)
@@ -150,7 +173,13 @@ int roundrobin()
     		assignQueues(time);
     		q1.front()[4]=0;
     		displayProcess(q1.front());
+    		cout<<"("<<time<<")";
     		q1.pop();
+    		if(timeRoundRobin==10)
+    		{
+    			priority();
+    			return 1;
+			}
 		}
 		else if(q1.front()[4]>tq)
 		{
@@ -159,10 +188,16 @@ int roundrobin()
 			assignQueues(time);
 			q1.front()[4]=q1.front()[4]-tq;
 			displayProcess(q1.front());
+			cout<<"("<<time<<")";
 			temp.push(q1.front());
 			q1.pop();
 			q1.push(temp.front());
 			temp.pop();
+			if(timeRoundRobin==10)
+			{
+				priority();
+				return 1;
+			}
 		}
 	    }
 	    else
@@ -172,6 +207,7 @@ int roundrobin()
 	    	assignQueues(time);
 	    	timeRoundRobin=10;
 	    	displayProcess(q1.front());
+	    	cout<<"("<<time<<")";
 	    	q1.front()[4]=q1.front()[4]-diff;
 	    	priority();
 	    	return 1;
@@ -236,9 +272,15 @@ int priority()
     	timePriority=timePriority+q2.front()[4];
     	q2.front()[4]=0;
     	displayProcess(q2.front());
+    	cout<<"("<<time<<")";
     	q2.pop();
     	assignQueues(time);
     	ordering(q2);
+    	if(timePriority==10)
+    	{
+    		fcfs();
+    		return 1;
+		}
         }
         else
         {
@@ -247,12 +289,95 @@ int priority()
         	assignQueues(time);
 	    	timePriority=10;
 	    	displayProcess(q2.front());
+	    	cout<<"("<<time<<")";
 	    	q2.front()[4]=q2.front()[4]-diff;
-	    	roundrobin();
+	    	fcfs();
 			return 1;
 		}
 	}
     }
-    roundrobin();	
+    fcfs();	
     return 0;
+}
+
+void orderingfcfs(queue<int*> q)
+{
+	int c[100][7];
+	int b[100][7];
+	queue<int*> g=q;
+	int i=0;
+	while(!g.empty())
+	{
+		for(int j=0;j<7;j++)
+		{
+			b[i][j]=g.front()[j];
+		}
+		i++;
+		g.pop();
+	}
+	for(int i=0;i<q.size();i++)
+	{
+		for(int j=0;j<7;j++)
+		{
+		c[i][j]=b[i][j];
+	    }
+	}
+	for(int k=0;k<i;k++)
+	{
+	    int min=c[0][2];
+	    int p=0;
+	for(int j=0;j<i;j++)
+	{
+		if(c[j][2]<min)
+		{
+			min=c[j][2];
+			p=j;
+		}
+	}
+	c[p][2]=10000;
+	g.push(b[p]);
+    }
+	q3=g;
+}
+
+int fcfs()
+{
+	assignQueues(time);
+	if(!q3.empty())
+	{
+		orderingfcfs(q3);
+		timeFcfs=0;
+		while(!q3.empty())
+		{
+			if(timeFcfs+q3.front()[4]<=10)
+			{
+			time=time+q3.front()[4];
+			timeFcfs=timeFcfs+q3.front()[4];
+			q3.front()[4]=0;
+			displayProcess(q3.front());
+			cout<<"("<<time<<")";
+			assignQueues(time);
+			orderingfcfs(q3);
+			q3.pop();
+			if(timeFcfs==10)
+			{
+				roundrobin();
+				return 1;
+			}
+		    }
+		   else
+		   {
+			int diff=10-timeFcfs;
+        	time=time+diff;
+        	timeFcfs=10;
+        	q3.front()[4]=q3.front()[4]-diff;
+        	displayProcess(q3.front());
+        	cout<<"("<<time<<")";
+			roundrobin();
+			return 1;
+		    }   
+	    }
+    }
+	roundrobin();
+	return 0;
 }
